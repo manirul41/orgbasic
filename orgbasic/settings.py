e27b70ab790340035bbb578eddeaa99038946611
manirul41 <manirul41@gmail.com>
2018-11-29 01:31:56 +0600

    init db

--- orgbasic/settings.py
+++ orgbasic/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'orgbasic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'orgbasicdb',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
