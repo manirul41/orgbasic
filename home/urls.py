7a12614e1472e8b4be9a0f025d047e3d3ab999ba
manirul41 <manirul41@gmail.com>
2018-12-05 00:50:12 +0600

    init login module.

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from home import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('about/', views.AboutView.as_view(), name='about'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,17 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
 
+@method_decorator(login_required(login_url='login/'), name='dispatch')
 class IndexView(TemplateView):
     template_name = "home/index.html"
+
+    #@login_required(login_url='/registration/login/')
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     return context
+
+
+class AboutView(TemplateView):
+    template_name = "about/about.html"
